933be910db2c114205aeb74d1c3784c318d70c70
harshnatuskar <harshnatuskar10@gmail.com>
2024-04-29 02:53:08 +0530

    added cameraViewModel

---
+++ app/src/main/java/com/example/cam/presentation/CameraViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.cam.presentation
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CameraViewModel @Inject constructor(){
+}
